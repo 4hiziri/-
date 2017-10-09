f1f525e5be4e086caae2e277838097155b0f7a72
4hiziri <meirvg@gmail.com>
2017-10-09 23:03:35 +0900

    Add parse function and use new class

--- checker.py
+++ checker.py
@@ -22,7 +22,14 @@ class Subject:
 
     def check(self):
         "足りない分を計算する"
-        return max(self.unit - self.border, 0)
+        return self.unit - self.border
+
+
+class Subject_Study(Subject):
+    graduation_study = 0
+
+    def __init__(self, unit, border, string):
+        pass
 
 
 class Special_Subject:
@@ -63,38 +70,54 @@ class Educational_Subject:
     science_cultural_subject = 0  # 理系教養科目
     zengaku_cultural_subject = 0  # 全学教養科目+開放科目
 
+    sum_unit_border = 55
+
     def __init__(self, summary_dict):
-        self.basic_subject = summary_dict['基礎セミナー'] + summary_dict['言語文化'] + \
-            summary_dict['健康・スポーツ科学（実習）'] + summary_dict['健康・スポーツ科学（講義）']
-        self.bunkei_subject = summary_dict['文系基礎科目'] + summary_dict['文系教養科目']
-        self.science_basic_subject = summary_dict['理系基礎科目']
-        self.science_cultural_subject = summary_dict['理系教養科目']
-        self.zengaku_cultural_subject = summary_dict['開放科目'] + \
-            summary_dict['全学教養科目']
+        unit = summary_dict['基礎セミナー']
+        unit += summary_dict['言語文化']
+        unit += summary_dict['健康・スポーツ科学（実習）']
+        unit += summary_dict['健康・スポーツ科学（講義）']
+        self.basic_subject = Subject(16., unit, '全学基礎科目')
+
+        unit = summary_dict['文系基礎科目'] + summary_dict['文系教養科目']
+        self.bunkei_subject = Subject(4., unit, "文系科目")
+
+        unit = summary_dict['理系基礎科目']
+        self.science_basic_subject = Subject(23., unit, "理系基礎科目")
+
+        unit = summary_dict['理系教養科目']
+        self.science_cultural_subject = Subject(4., unit, "理系教養科目")
+
+        unit = summary_dict['開放科目'] + summary_dict['全学教養科目']
+        self.zengaku_cultural_subject = Subject(2., unit, "全学教養科目")
+
+        return
 
     def check(self):
-        insufficient_subject = []
+        insufficient_subjects = []
 
-        def append_if_lt(subject_score, needed_score, subject_str):
-            if subject_score < needed_score:
-                insufficient_subject.append((subject_str,
-                                             needed_score - subject_score))
+        def append_subject(subject):
+            insufficient_subjects.append((str(subject), subject.check()))
 
-        append_if_lt(self.basic_subject, 16, "全学基礎科目")
-        append_if_lt(self.bunkei_subject, 4, "文系科目")
-        append_if_lt(self.science_basic_subject, 23, "理系基礎科目")
-        append_if_lt(self.science_cultural_subject, 4, "理系教養科目")
-        append_if_lt(self.zengaku_cultural_subject, 2, "全学教養科目")
+        append_subject(self.basic_subject)
+        append_subject(self.bunkei_subject)
+        append_subject(self.science_basic_subject)
+        append_subject(self.science_cultural_subject)
+        append_subject(self.zengaku_cultural_subject)
 
-        sum = self.basic_subject + self.bunkei_subject + self.science_basic_subject + \
-            self.science_cultural_subject + self.zengaku_cultural_subject
-        append_if_lt(sum, 55, "合計")
+        sum = self.basic_subject
+        sum += self.bunkei_subject
+        sum += self.science_basic_subject
+        sum += self.science_cultural_subject
+        sum += self.zengaku_cultural_subject
 
-        return insufficient_subject
+        insufficient_subjects(('合計', sum - self.sum_unit_border))
+
+        return insufficient_subjects
 
 
 # HACK: dirty work
-class Subject:
+class Score:
     special_subject = None
     educational_subject = None
 
@@ -106,11 +129,25 @@ class Subject:
         return self.special_subject.check() + self.educational_subject.check()
 
 
-def table_dict(bs_table):
+def scores_dict(bs_table):
     td_list = map(lambda tr: tr.find_all('td'), bs_table.find_all('tr'))
     return dict(
         map(lambda tds: (tds[0].text.strip()[1:], float(tds[1].text.strip())),
             td_list))
+
+
+def subject_table_dict(table):
+    def to_tuple(tr):
+        td = tr.find_all('td')
+        name = td[0].text
+        unit = float(td[2].text)
+        return (name, unit)
+
+    filter_null = filter(lambda tr: len(tr.find_all('td'))
+                         != 0, table.find_all('tr'))
+    filter_title = filter(lambda tr: tr.find_all('td')
+                          [2].text != '　', filter_null)
+    return dict(map(to_tuple, filter_title))
 
 
 # table[1]
@@ -147,7 +184,12 @@ def subjects_to_dict(subjects):
 
 def get_summary_score(url):
     bs = BeautifulSoup(requests.get(url).text, "html.parser")
-    return Subject(table_dict(bs.find_all('table')[2]))
+    tables = bs.find_all('table')
+    scores = scores_dict(tables[2])
+    subjects = subject_table_dict(tables[1])
+    scores.update(subjects)
+
+    return Score(scores)
 
 
 def table_to_trs(table):
@@ -156,7 +198,6 @@ def table_to_trs(table):
 
 
 def split_subject(tables):
-    print(len(tables))
     return (table_to_trs(tables[0]), table_to_trs(tables[1]),
             table_to_trs(tables[2]))
 
@@ -170,7 +211,7 @@ def regulate_subjects(subject_list):
         subject_info = re.search(r'\(.*\)', tds[4].text).group()[1:-1]
         return (subject_name, subject_info)
 
-    return list(map(regulate, subject_list))
+    return dict(map(regulate, subject_list))
 
 
 def parse_syllabus(url):
